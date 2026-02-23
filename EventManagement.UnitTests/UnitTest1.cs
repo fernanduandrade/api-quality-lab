fc66df7238584bab6cdbd2761e2d6e2e70ea1361
fernanduandrade <onanduandrade@gmail.com>
2026-02-22 21:13:49 -0300

    tests: add failed test

--- EventManagement.UnitTests/UnitTest1.cs
+++ EventManagement.UnitTests/UnitTest1.cs
@@ -2,9 +2,17 @@
 
 public class UnitTest1
 {
-    [Fact]
+    [Fact(DisplayName = "Should pass this test")]
+    [Trait("Category", "Success")]
     public void Test1()
     {
         Assert.True(true);
+    }
+
+    [Fact(DisplayName = "Should fail this test")]
+    [Trait("Category", "Failure")]
+    public void Test2()
+    {
+        Assert.True(false);
     }
 }
